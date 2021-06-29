dee50c7159c65ebe03535d1d7b79a2756d94d988
Choi Gwangrim <rhkdfladl11@naver.com>
2021-06-29 21:11:31 +0900

    test001

--- src/hellow.js
+++ src/hellow.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    hellows
+    test001
     <App />
   </React.StrictMode>,
   document.getElementById('root')
