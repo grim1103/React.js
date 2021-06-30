7a2b43ea4575f1b4bffd50c116e44bf611055c0c
Choi Gwangrim <rhkdfladl11@naver.com>
2021-06-30 14:46:43 +0900

    test002

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React,{ Comment } from 'react';
 import './App.css';
-import { render } from '@testing-library/react';
 
 
 
@@ -8,44 +7,11 @@ class App extends Comment{
 render(){
   return (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+          Hellow
         </div>
       );
-}
+  }
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
