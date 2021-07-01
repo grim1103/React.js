694a4af3c04e75c37eef71b7e96feccae606efcd
Choi Gwangrim <rhkdfladl11@naver.com>
2021-07-01 13:36:21 +0900

    Component를 파일로 분리

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
-
-class Subject extends Component{
-  render(){
-    return(
-      <header>
-       <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component{
-  render(){
-    return(
-      <nav>
-          <ul>
-              <li><a href="1.html"> HTML </a></li>
-              <li><a href="2.html"> CSS </a></li>
-              <li><a href="3.html"> JavaScript </a></li>
-          </ul>
-        </nav>
-    );
-  }
-}
-
-class Content extends Component{
-  render(){
-    return(
-          <article>
-            <h2>{this.props.title}</h2>
-            {this.props.desc}
-          </article>
-    );
-  }
-}
-
+import TOC from './conponents/TOC';
+import Subject from './conponents/Subject';
+import Content from './conponents/Content';
 
 
 class App extends Component{
---
+++ src/conponents/TOC.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class TOC extends Component{
+    render(){
+      return(
+        <nav>
+            <ul>
+                <li><a href="1.html"> HTML1 </a></li>
+                <li><a href="2.html"> CSS 2</a></li>
+                <li><a href="3.html"> JavaScript 3</a></li>
+                <li><a href="3.html"> JavaScript 4</a></li>
+            </ul>
+          </nav>
+      );
+    }
+  }
+
+  export default TOC;
