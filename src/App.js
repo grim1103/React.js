dd50e3e7c805c2b3f302296cd1c2830314fbd02e
Choi Gwangrim <rhkdfladl11@naver.com>
2021-07-01 13:14:44 +0900

    Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import React, {Component} from 'react';
 import './App.css';
 
-class Subject extends Comment{
+class Subject extends Component{
   render(){
     return(
       <header>
-       <h1>Web</h1>
-        World Wide Web
+       <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
     );
   }
 }
 
+class TOC extends Component{
+  render(){
+    return(
+      <nav>
+          <ul>
+              <li><a href="1.html"> HTML </a></li>
+              <li><a href="2.html"> CSS </a></li>
+              <li><a href="3.html"> JavaScript </a></li>
+          </ul>
+        </nav>
+    );
+  }
+}
+
+class Content extends Component{
+  render(){
+    return(
+          <article>
+            <h2>{this.props.title}</h2>
+            {this.props.desc}
+          </article>
+    );
+  }
+}
+
+
+
 class App extends Component{
   render(){
     return (
           <div className="App">
-            <Subject></Subject>
+            <Subject title="WEB" sub="World Wide Web zz"></Subject>
+            <Subject title="React" sub="React.js"></Subject>
+            <TOC></TOC>
+            <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
           </div>
         );
   }
